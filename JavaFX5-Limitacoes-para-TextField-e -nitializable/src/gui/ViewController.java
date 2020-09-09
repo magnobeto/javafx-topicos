86ce48380c55a61f20102815f09a27efd7855593
Roberto Magno <magnobeto.rm@gmail.com>
2020-09-09 20:09:20 -0300

    Atualização

---
+++ JavaFX5-Limitacoes-para-TextField-e -nitializable/src/gui/ViewController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import gui.util.Alerts;
+import gui.util.Constraints;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Roberto
+ */
+public class ViewController implements Initializable {
+
+    @FXML
+    private TextField txtNumber1;
+
+    @FXML
+    private TextField txtNumber2;
+
+    @FXML
+    private Label labelResult;
+
+    @FXML
+    private Button btSum;
+
+    @FXML
+    public void onBtSumtAction() {
+        try {
+            Locale.setDefault(Locale.US);
+            double number1 = Double.parseDouble(txtNumber1.getText());
+            double number2 = Double.parseDouble(txtNumber2.getText());
+            double sum = number1 + number2;
+            labelResult.setText(String.format("%.2f", sum));
+        }
+        catch (NumberFormatException e){
+            Alerts.showAlert("ERROR!", "Parse error", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Constraints.setTextFieldDouble(txtNumber1);
+        Constraints.setTextFieldDouble(txtNumber2);
+        Constraints.setTextFieldMaxLength(txtNumber1, 12);
+        Constraints.setTextFieldMaxLength(txtNumber2, 12);
+    }
+}
